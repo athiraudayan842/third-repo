a823e0e25838cefa82d29eb1854a0f0d458d896a
Admin <Admin@DESKTOP-5IN5P6J>
2024-12-31 21:11:37 +0530

    third commit

---
+++ Sampleproject/src/main/java/simple/Sample9.java
@@ -0,0 +1,16 @@
+package simple;
+
+public class Sample9 {
+
+	public static void main(String[] args) {
+	Sample9 obj=new Sample9();
+	obj.sample1(3,4);
+	}
+	public void sample1(int a,int b)
+	{
+		int sum=a+b;
+		System.out.println(sum);
+
+	}
+
+}
